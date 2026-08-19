d0969b0e549334249e45fc20806e66f137de3b1c
Mohammadreza <developerMammad@proton.me>
2026-08-19 05:59:49 +0330

    feat(config): Setup configuration for project.
    Install config package. configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+      load: [appConfig],
+    }),
+  ],
 })
 export class AppModule {}
